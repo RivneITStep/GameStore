76f738f47e869ace22ad56c90f75c8f773f0bacb
sashapena1337 <sashapena1337@gmail.com>
2021-02-15 15:08:12 +0200

    Done Token

---
+++ CourseworkDomain/Interfaces/IJWTTokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Entity.Role;
+
+namespace CourseworkDomain.Interfaces
+{
+    public interface IJWTTokenService
+    {
+        string CreateToken(User user);
+
+    }
+}
---
+++ CourseworkDomain/JWTTokenService.cs
@@ -0,0 +1,62 @@
+﻿using CourseworkDomain.Interfaces;
+using DataAccess;
+using DataAccess.Entity.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace CourseworkDomain
+{
+    public class JWTTokenService : IJWTTokenService
+    {
+            private readonly EFContext _context;
+            private readonly IConfiguration _configuration;
+            private readonly UserManager<User> _userManager;
+            public JWTTokenService(EFContext context,
+                IConfiguration configuration,
+                UserManager<User> userManager)
+            {
+                _context = context;
+                _configuration = configuration;
+                _userManager = userManager;
+            }
+
+            public string CreateToken(User user)
+            {
+                var roles = _userManager.GetRolesAsync(user).Result;
+                //var fullName = _context.UserMoreInfos.FirstOrDefault(t => t.id == user.Id).FullName;
+                var claims = new List<Claim>()
+            {
+                //new Claim(JwtRegisteredClaimNames.Sub, user.Id)
+                new Claim("id", user.Id.ToString()),
+                //new Claim("name", fullName),
+                new Claim("email", user.Email)
+            };
+
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim("roles", role));
+                }
+
+                string jwtTokenSecretKey = this._configuration.GetValue<string>("SecretPhrase");
+
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenSecretKey));
+                var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+                var jwt = new JwtSecurityToken(
+                    signingCredentials: signingCredentials,
+                    claims: claims,
+                    expires: DateTime.Now.AddYears(1));
+
+                return new JwtSecurityTokenHandler().WriteToken(jwt);
+            }
+
+        
+
+    }
+}
